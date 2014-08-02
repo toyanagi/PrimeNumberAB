68cc3db2e7f4df78f9b926ab6f92aace44d408b9
toyanagi <t.oyanagi@gmail.com>
2014-08-03 05:58:36 +0900

    固定した桁数内で10個の候補A,Bを探索

--- src/hogehoge/com/PrimeNumAB.java
+++ src/hogehoge/com/PrimeNumAB.java
@@ -72,67 +72,74 @@ public class PrimeNumAB {
 		System.out.println("PrimeNum B Length = " + PrimeNumBLength );
 		
 		//Aを探索する
-		PrimeNumA = (long)Math.pow(10, PrimeNumALength) -1;
-		PrimeNumA = previousPrimeNum(PrimeNumA, 0);
+		PrimeNumA = (long)Math.pow(10, PrimeNumALength);
 		
-		System.out.println("PrimeNumA2  = " + PrimeNumA );
-
-		//Bを探索する		
-		//A、Bの桁数が同じ／異なるケースで場合分け
-		if(PrimeNumALength==PrimeNumBLength){
-			PrimeNumB = PrimeNumA - 1;                            //AとBの桁数が同じ 場合、(確定したA）-1 をセット
-		}else{
-			PrimeNumB = (long)Math.pow(10, PrimeNumBLength) - 1;    //AとBの桁数が異なる 場合、B^10-1  をセット
-		}		
-				
-		//次に小さい素数を探す
-		PrimeNumB = previousPrimeNum(PrimeNumB, 0);
-			
-		//第一候補とする
-		CompositeNumN = PrimeNumA * PrimeNumB;
-		System.out.println("PrimeNumB(1st candidate)  = " + PrimeNumB 
-				           		 + "   N = " + CompositeNumN);
-		//候補が現在のN以上か
-		if(CompositeNumN >= CompositeNumNLast){
-			PrimeNumALast = PrimeNumA;
-			PrimeNumBLast = PrimeNumB;
-			CompositeNumNLast = CompositeNumN;
-			System.out.println("CompositeNumN(1st candidate is optimum solution) = " +
-					 CompositeNumNLast + " = " + PrimeNumALast + 
-					" * " + PrimeNumBLast );
-		}
-				
-		//ループ３（B探索 第２候補）開始
-		//N桁となる最大のB２を求める
-		PrimeNumB2= ((long)Math.pow(10, CompositeNumNLength)-1) / PrimeNumA;
-		CompositeNumN2 = PrimeNumA * PrimeNumB2;
-		System.out.println("PrimeNumB(2nd candidate(max))  = " + PrimeNumB2 +
-						"   N = " + CompositeNumN2 );
-				
-		//候補N2が現在のN以下か
-		if( CompositeNumN2 <= CompositeNumNLast){
-		//以下の場合
-			//B、Nを確定（何もしない）
-			System.out.println("PrimeNumB(2nd candidate(max)) is no candidate");
-		//大きい場合
-		}else{
-			//１つ小さい素数を探す
-			PrimeNumB2 = previousPrimeNum(PrimeNumB2, 0);
-			CompositeNumN2 = PrimeNumA * PrimeNumB2;
-					
-			//候補が現在のN以上か
-			if(CompositeNumN2 >= CompositeNumNLast){
-			//B2をBとして確定
-				PrimeNumALast = PrimeNumA;
-				PrimeNumBLast = PrimeNumB2;
-				CompositeNumNLast = CompositeNumN2; 
-				System.out.println("PrimeNumB(2nd candidate is optimum solution)  = " + PrimeNumBLast +
-								"   N = " + CompositeNumNLast );
+		//A,Bを探索する
+		for(int i=0;i<=10;i++){
+			PrimeNumA--;
+			PrimeNumA = previousPrimeNum(PrimeNumA, 0);
+		
+			System.out.println("PrimeNumA2  = " + PrimeNumA );
+	
+			//Bを探索する		
+			//A、Bの桁数が同じ／異なるケースで場合分け
+			if(PrimeNumALength==PrimeNumBLength){
+				PrimeNumB = PrimeNumA - 1;                            //AとBの桁数が同じ 場合、(確定したA）-1 をセット
 			}else{
-				System.out.println("PrimeNumB(2nd candidate is no optimum solution)  = " + PrimeNumB2 +
-								"   N = " + CompositeNumN2 );
+				PrimeNumB = (long)Math.pow(10, PrimeNumBLength) - 1;    //AとBの桁数が異なる 場合、B^10-1  をセット
+			}		
+					
+			//次に小さい素数を探す
+			PrimeNumB = previousPrimeNum(PrimeNumB, 0);
+				
+			//第一候補とする
+			CompositeNumN = PrimeNumA * PrimeNumB;
+			System.out.println("PrimeNumB(1st candidate)  = " + PrimeNumB 
+					           		 + "   N = " + CompositeNumN);
+			//候補が現在のN以上か
+			if(CompositeNumN >= CompositeNumNLast){
+				PrimeNumALast = PrimeNumA;
+				PrimeNumBLast = PrimeNumB;
+				CompositeNumNLast = CompositeNumN;
+				System.out.println("CompositeNumN(1st candidate is optimum solution) = " +
+						 CompositeNumNLast + " = " + PrimeNumALast + 
+						" * " + PrimeNumBLast );
 			}
-		}	
+					
+			//ループ３（B探索 第２候補）開始
+			//N桁となる最大のB２を求める
+			PrimeNumB2= ((long)Math.pow(10, CompositeNumNLength)-1) / PrimeNumA;
+			CompositeNumN2 = PrimeNumA * PrimeNumB2;
+			System.out.println("PrimeNumB(2nd candidate(max))  = " + PrimeNumB2 +
+							"   N = " + CompositeNumN2 );
+					
+			//候補N2が現在のN以下か
+			if( CompositeNumN2 <= CompositeNumNLast){
+			//以下の場合
+				//B、Nを確定（何もしない）
+				System.out.println("PrimeNumB(2nd candidate(max)) is no candidate");
+			//大きい場合
+			}else{
+				//１つ小さい素数を探す
+				PrimeNumB2 = previousPrimeNum(PrimeNumB2, 0);
+				CompositeNumN2 = PrimeNumA * PrimeNumB2;
+						
+				//候補が現在のN以上か
+				if(CompositeNumN2 >= CompositeNumNLast){
+				//B2をBとして確定
+					PrimeNumALast = PrimeNumA;
+					PrimeNumBLast = PrimeNumB2;
+					CompositeNumNLast = CompositeNumN2; 
+					System.out.println("PrimeNumB(2nd candidate is optimum solution)  = " + PrimeNumBLast +
+									"   N = " + CompositeNumNLast );
+				}else{
+					System.out.println("PrimeNumB(2nd candidate is no optimum solution)  = " + PrimeNumB2 +
+									"   N = " + CompositeNumN2 );
+				}
+			}
+			
+			//次のA、Bを探す
+		}
 		
 	}
 	
